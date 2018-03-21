f1b5acf2cebdc479abe286f2c780178ea68d9359
OzanOzer <37022739+OzanOzer@users.noreply.github.com>
2018-03-21 23:07:26 +0300

    Update Programlama I(2. ödev).py

--- Programlama I(2. ödev).py
+++ Programlama I(2. ödev).py
@@ -1,10 +1,5 @@
 #1.Soru
-def katmaDegerCiroHesapla():
-    a=int(input('Toplam Satış Miktarını giriniz:'))
-    b=int(input('Hammadde Maliyetini giriniz:'))
-    c=int(input('Bakım Onarım Giderlerini giriniz:'))
-    d=int(input('Sevkiyat giderlerini giriniz:'))
-    e=int(input('Satın alınan hizmet gierlerini giriniz:'))
+def katmaDegerCiroHesapla(a,b,c,d,e):
     katmaDegerCiro=a-(b+c+d+e)
     if (katmaDegerCiro>1000):
         print('Katma değer cironuz:',katmaDegerCiro,'İşletme katma değer cirosu yüksek.')
@@ -12,6 +7,12 @@ def katmaDegerCiroHesapla():
         print('Katma değer cironuz:',katmaDegerCiro,'İşletme katma değer cirosu normal.')
     else:
         print('Katma değer cironuz:',katmaDegerCiro,'işletme katma değer cirosu düşük.')
+a=int(input('Toplam Satış Miktarını giriniz:'))
+b=int(input('Hammadde Maliyetini giriniz:'))
+c=int(input('Bakım Onarım Giderlerini giriniz:'))
+d=int(input('Sevkiyat giderlerini giriniz:'))
+e=int(input('Satın alınan hizmet gierlerini giriniz:'))
+katmaDegerCiroHesapla(a,b,c,d,e)
 ##2.soru
 #calSur:Çalışma Süresi
 #topMusSa:Toplam müşteri sayısı
